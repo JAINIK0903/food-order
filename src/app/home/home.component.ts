a6febbba0472d0cf0c6e355ff333ea59955f2509
Jainik090302 <git@github.com:Jainik090302/online-shoping.git>
2023-07-27 16:29:54 +0530

    implement search functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Foods } from 'src/app/shared/models/food';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +13,16 @@ export class HomeComponent implements OnInit {
   max = 10;
   rate = 5;
   isReadonly = true;
-  constructor(private _foodService: FoodService) { }
+  constructor(private _foodService: FoodService, private router: ActivatedRoute) { }
   ngOnInit(): void {
-    this.foods = this._foodService.getAll();
+    this.router.params.subscribe(params => {
+      if (params['searchItem'])
+        this.foods = this._foodService.getAll().filter(food =>
+          food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
+      else
+        this.foods = this._foodService.getAll();
+    })
+
   }
 
 }
