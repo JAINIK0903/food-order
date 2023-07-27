6e35b905a4563c92046f1a38583e0c13746f0bc4
Jainik090302 <git@github.com:Jainik090302/online-shoping.git>
2023-07-27 11:40:58 +0530

    apply getall method on food service

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -6,4 +6,17 @@ import { Injectable } from '@angular/core';
 export class FoodService {
 
   constructor() { }
+
+  getAll(): string[] {
+    return [
+      '/assets/food-1.jpg',
+      '/assets/food-2.jpg',
+      '/assets/food-3.jpg',
+      '/assets/food-4.jpg',
+      '/assets/food-5.jpg',
+      '/assets/food-6.jpg',
+      '/assets/food-7.jpg',
+      '/assets/food-8.jpg',
+    ]
+  }
 }
