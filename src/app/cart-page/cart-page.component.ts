ee766bd50b282380760e5ae1ceeed65e2b88957d
Jainik090302 <git@github.com:Jainik090302/online-shoping.git>
2023-07-28 13:11:58 +0530

    implement cart page

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cart } from '../shared/models/cart';
+import { CartService } from '../services/food/cart.service';
+import { CartItem } from '../shared/models/cartItem';
+import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.scss']
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
+  cart!: Cart
+  constructor(private cartService: CartService, private foodService: FoodService) {
+    let foods = foodService.getAll();
+    cartService.addToCart(foods[1]);
+    cartService.addToCart(foods[3]);
+    cartService.addToCart(foods[5]);
+    this.setCart();
+  }
+  ngOnInit(): void {
+  }
+  setCart() {
+    this.cart = this.cartService.getCart();
+  }
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+    this.setCart();
+  }
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+    this.setCart();
+  }
 
 }
